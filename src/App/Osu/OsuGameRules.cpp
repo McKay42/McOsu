2e6ae2797911083d8eb2d090579a303d852d4c7b
Jason Foley <JTF195@hotmail.com>
2017-04-10 09:48:51 -0400

    Revert "Tiny fix to the default hitwindow values."
    This reverts commit acd9fbf1cef692bc45b9b93c13f3bbc4acc84cb2.

--- src/App/Osu/OsuGameRules.cpp
+++ src/App/Osu/OsuGameRules.cpp
@@ -31,16 +31,16 @@ ConVar OsuGameRules::osu_mod_millhioref("osu_mod_millhioref", false);
 ConVar OsuGameRules::osu_mod_millhioref_multiplier("osu_mod_millhioref_multiplier", 2.0f);
 
 // all hitwindow values here are in milliseconds
-ConVar OsuGameRules::osu_hitwindow_300_min("osu_hitwindow_300_min", 79.5f);
-ConVar OsuGameRules::osu_hitwindow_300_mid("osu_hitwindow_300_mid", 49.5f);
-ConVar OsuGameRules::osu_hitwindow_300_max("osu_hitwindow_300_max", 19.5f);
+ConVar OsuGameRules::osu_hitwindow_300_min("osu_hitwindow_300_min", 80);
+ConVar OsuGameRules::osu_hitwindow_300_mid("osu_hitwindow_300_mid", 50);
+ConVar OsuGameRules::osu_hitwindow_300_max("osu_hitwindow_300_max", 20);
 
-ConVar OsuGameRules::osu_hitwindow_100_min("osu_hitwindow_100_min", 139.5f);
-ConVar OsuGameRules::osu_hitwindow_100_mid("osu_hitwindow_100_mid", 99.5f);
-ConVar OsuGameRules::osu_hitwindow_100_max("osu_hitwindow_100_max", 59.5f);
+ConVar OsuGameRules::osu_hitwindow_100_min("osu_hitwindow_100_min", 140);
+ConVar OsuGameRules::osu_hitwindow_100_mid("osu_hitwindow_100_mid", 100);
+ConVar OsuGameRules::osu_hitwindow_100_max("osu_hitwindow_100_max", 60);
 
-ConVar OsuGameRules::osu_hitwindow_50_min("osu_hitwindow_50_min", 199.5f);
-ConVar OsuGameRules::osu_hitwindow_50_mid("osu_hitwindow_50_mid", 149.5f);
-ConVar OsuGameRules::osu_hitwindow_50_max("osu_hitwindow_50_max", 99.5f);
+ConVar OsuGameRules::osu_hitwindow_50_min("osu_hitwindow_50_min", 200);
+ConVar OsuGameRules::osu_hitwindow_50_mid("osu_hitwindow_50_mid", 150);
+ConVar OsuGameRules::osu_hitwindow_50_max("osu_hitwindow_50_max", 100);
 
-ConVar OsuGameRules::osu_hitwindow_miss("osu_hitwindow_miss", 400.0f);
+ConVar OsuGameRules::osu_hitwindow_miss("osu_hitwindow_miss", 400);
