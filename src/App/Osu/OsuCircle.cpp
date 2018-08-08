960a0944839a201aa005ae73454e67cebb486094
McKay42 <->
2018-08-08 14:14:18 +0200

    See https://github.com/McKay42/McOsu/commit/ad5efd8fe0ab14412825271d151772b263f96014 Positional audio

--- src/App/Osu/OsuCircle.cpp
+++ src/App/Osu/OsuCircle.cpp
@@ -628,7 +628,9 @@ void OsuCircle::onHit(OsuScore::HIT result, long delta, float targetDelta, float
 		if (m_osu_timingpoints_force->getBool())
 			m_beatmap->updateTimingPoints(m_iTime);
 
-		m_beatmap->getSkin()->playHitCircleSound(m_iSampleType);
+		const Vector2 osuCoords = m_beatmap->pixels2OsuCoords(m_beatmap->osuCoords2Pixels(m_vRawPos));
+
+		m_beatmap->getSkin()->playHitCircleSound(m_iSampleType, OsuGameRules::osuCoords2Pan(osuCoords.x));
 
 		m_fHitAnimation = 0.001f; // quickfix for 1 frame missing images
 		anim->moveQuadOut(&m_fHitAnimation, 1.0f, OsuGameRules::getFadeOutTime(m_beatmap), true);
